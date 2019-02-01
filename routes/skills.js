338052858fb4ea743c304d8bcc7edbcc164c4c8a
eric-couch <James.Couch@edgetechacademy.edu>
2019-01-31 19:58:54 -0600

    added config string to hide db auth

--- routes/skills.js
+++ routes/skills.js
@@ -1,22 +1,20 @@
 var express = require("express");
 var router = express.Router();
 var MongoClient = require("mongodb").MongoClient;
+const mongouri = require("../config/keys").MongoURI;
 
 router.all("/", function(req, res, next) {
-  MongoClient.connect(
-    "mongodb://webroot:iSYSk7alJQCdDXnDgIA8nRaMc6d4GhZA9VV1gCe7CmrD57XRdajqm4B3y6A6X0FAeOdwLc7zEsBUROsHfHDMjA%3D%3D@webroot.documents.azure.com:10255/?ssl=true",
-    function(err, db) {
-      var dbo = db.db("modern_portfolio");
-      dbo
-        .collection("skills")
-        .find()
-        .toArray(function(err, result) {
-          if (err) throw err;
-          res.json(result);
-          db.close();
-        });
-    }
-  );
+  MongoClient.connect(mongouri, function(err, db) {
+    var dbo = db.db("modern_portfolio");
+    dbo
+      .collection("skills")
+      .find()
+      .toArray(function(err, result) {
+        if (err) throw err;
+        res.json(result);
+        db.close();
+      });
+  });
 });
 
 module.exports = router;
